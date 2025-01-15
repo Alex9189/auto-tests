9a8648158306752ec1d28fb6ca717bfaf5489c21
av.stecenko <av.stecenko@tensor.ru>
2025-01-15 16:07:50 +0300

    операторы условий

---
+++ Доброски/If elif (stage4).py
@@ -0,0 +1,37 @@
+# my_age = 16
+# if my_age < 17:
+#     print('Ura ich bin Schüler')
+# elif my_age == 23:
+#     print('Мне 23')
+# elif my_age > 70:
+# #     print('wo ist mein autohaus')
+# # elif my_age < 25:
+# #     print('ich bin noch in der Uni')
+# # else:
+# #     print('О 9 утра! Пора на работу!')
+# a = 2010
+# b = 1500
+# # if a < b:
+# #     rez = a + b
+# # else:
+# #     rez = a - b
+# rez = a + b if a < b else a - b # аналогично вышеуказанному циклу
+# print(rez)
+# my_list = [11, ['asdsa', 'asd'], 123, 124, 355]
+# for i in range(7):
+#     if i == 5:
+#         continue
+#     print(i)
+# else:
+#     print('Мы заввершили цикл')
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         break
+#     elif i == 3:
+#         continue
+#     i += 1
+#     print(i)
+a = None
+print(type(a))
+
